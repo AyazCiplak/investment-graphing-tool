95852a0dbe8b6008f6351c842ccf1cba2c168339
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-03-14 13:18:19 -0400

    Internal flex containers added for graph bars

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ function displayValues() {
 
     const initialAmount = getValue("initial-investment");
     const years = getValue("years");
-    const interestRate = getValue("interest-rate");
+    const interestRate = getValue("interest-rate")/100;
     const recurring = getValue("recurring-amount");
+    //Retrieves selected radio input option
+    const frequency = parseFloat(document.querySelector('input[name="contribution-frequency"]:checked').value);
 
-    document.getElementById("final-amount").innerHTML = "$";
+    //Computes future value of investment with compound interest formula
+    
 
 };

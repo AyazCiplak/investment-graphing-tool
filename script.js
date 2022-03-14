e4c7e63bfefd10db588fd2e5dceb6468e3090574
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-03-14 13:19:12 -0400

    Added function for final investment value calculation and display

--- script.js
+++ script.js
@@ -12,6 +12,11 @@ function displayValues() {
     const frequency = parseFloat(document.querySelector('input[name="contribution-frequency"]:checked').value);
 
     //Computes future value of investment with compound interest formula
-    
+    const futureValue = (initialAmount*((1+interestRate)**years) + recurring*frequency*(((1+interestRate)**years)-1)/interestRate).toFixed(2);
+
+    const xValues = ["x-value-1", "x-value-2", "x-value-3", "x-value-4", "x-value-5", "x-value-6", "x-value-7", "x-value-8"];
+    const yValues = ["y-value-1", "y-value-2", "y-value-3", "y-value-4", "y-value-5"];
+
+    document.getElementById("final-amount").innerHTML = futureValue + " $"
 
 };

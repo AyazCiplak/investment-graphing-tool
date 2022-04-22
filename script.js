fddf32acd1b6348483252dadeab894a6491d9218
AyazCiplak <97311304+AyazCiplak@users.noreply.github.com>
2022-04-21 21:30:08 -0400

    Modified JavaScript code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Retrieves values from input forms
 const getValue = formValue => parseFloat(document.getElementById(formValue).value);
+const getClass = (className, occurrence) => document.getElementsByClassName(className)[occurrence].innerHTML;
 
 function setAxes() {
 
@@ -21,6 +22,7 @@ function setAxes() {
     
     //For both graphs, finds and displays x-axis increments based on years, and y-axis increments based on final investment value
     for (let i = 0; i < xValues.length; i++){
+        
         document.getElementsByClassName(xValues[i])[0].innerHTML = findIncrement(years, xValues.length, i+1);
         document.getElementsByClassName(xValues[i])[1].innerHTML = findIncrement(years, xValues.length, i+1);
     };
@@ -32,53 +34,31 @@ function setAxes() {
     
     //Displays final value of investment
     if (futureValue == "NaN") {
-        document.getElementById("final-amount").innerHTML = "Invalid input"
+        document.getElementById("final-amount").innerHTML = "Invalid Input"
     } else {
     document.getElementById("final-amount").innerHTML = futureValue + " $"
     }
-};
 
-function setTotalBars() {
 
-    const initialAmount = getValue("initial-investment");
-    const years = getValue("years");
-    const interestRate = getValue("interest-rate")/100;
-    const recurring = getValue("recurring-amount");
-    //Retrieves selected radio input option
-    const frequency = parseFloat(document.querySelector('input[name="contribution-frequency"]:checked').value);
 
-    const totalBarValues = [
-        "bar-1", 
-        "bar-2", 
-        "bar-3", 
-        "bar-4", 
-        "bar-5", 
-        "bar-6", 
-        "bar-7", 
-        "bar-8"
-    ];
 
-    const contributionBarValues = [
-        "contribution-bar-1",
-        "contribution-bar-2", 
-        "contribution-bar-3", 
-        "contribution-bar-4", 
-        "contribution-bar-5", 
-        "contribution-bar-6", 
-        "contribution-bar-7", 
-        "contribution-bar-8"
-    ];
 
-    const interestBarValues = [
-        "interest-bar-1", 
-        "interest-bar-2", 
-        "interest-bar-3", 
-        "interest-bar-4", 
-        "interest-bar-5", 
-        "interest-bar-6", 
-        "interest-bar-7", 
-        "interest-bar-8"
-    ];
+
+
+
+
+
+
+
+
+
+    const totalBarValues = ["bar-1", "bar-2", "bar-3", "bar-4", "bar-5", "bar-6", "bar-7", "bar-8"];
+
+    const contributionBarValues = ["contribution-bar-1","contribution-bar-2", "contribution-bar-3", "contribution-bar-4", 
+        "contribution-bar-5", "contribution-bar-6", "contribution-bar-7", "contribution-bar-8"];
+
+    const interestBarValues = ["interest-bar-1", "interest-bar-2", "interest-bar-3", "interest-bar-4", "interest-bar-5", 
+        "interest-bar-6",  "interest-bar-7",  "interest-bar-8"];
 
 //Computes future value of investment with compound interest formula
     const maxInvestmentValue = initialAmount*((1+interestRate)**years) + recurring*frequency*(((1+interestRate)**years)-1)/interestRate;
@@ -117,4 +97,61 @@ function setTotalBars() {
         const currentValue = interestValueAtIncrement(i+1, interestBarValues.length);
         document.getElementById(interestBarValues[i]).style.height = heightProportion(currentValue);
     }
-}
+
+};
+
+/*function setTotalBars() {
+
+    const initialAmount = getValue("initial-investment");
+    const years = getValue("years");
+    const interestRate = getValue("interest-rate")/100;
+    const recurring = getValue("recurring-amount");
+    //Retrieves selected radio input option
+    const frequency = parseFloat(document.querySelector('input[name="contribution-frequency"]:checked').value);
+
+    const totalBarValues = ["bar-1", "bar-2", "bar-3", "bar-4", "bar-5", "bar-6", "bar-7", "bar-8"];
+
+    const contributionBarValues = ["contribution-bar-1","contribution-bar-2", "contribution-bar-3", "contribution-bar-4", 
+        "contribution-bar-5", "contribution-bar-6", "contribution-bar-7", "contribution-bar-8"];
+
+    const interestBarValues = ["interest-bar-1", "interest-bar-2", "interest-bar-3", "interest-bar-4", "interest-bar-5", 
+        "interest-bar-6",  "interest-bar-7",  "interest-bar-8"];
+
+//Computes future value of investment with compound interest formula
+    const maxInvestmentValue = initialAmount*((1+interestRate)**years) + recurring*frequency*(((1+interestRate)**years)-1)/interestRate;
+
+    //Finds total progression of investment at any increment of time
+    function totalValueAtIncrement(increment, totalIncrements) {
+        const incrementFraction = increment/totalIncrements;
+        return (initialAmount*((1+interestRate)**(years*incrementFraction))) + ((recurring*frequency)*(((1+interestRate)**(years*incrementFraction))-1))/interestRate;
+    };
+
+    //Computes height percentage of each bar based on the maximum (final) bar's height
+    const heightProportion = (value) => (value / maxInvestmentValue * 100) + "%";
+
+    //Finds proportion of each bar based on the max. value
+    for (let i = 0; i < totalBarValues.length; i++){
+        const currentValue = totalValueAtIncrement(i+1, totalBarValues.length);
+        document.getElementById(totalBarValues[i]).style.height = heightProportion(currentValue);
+    };
+
+    //Finds net contributions at any increment of time
+    function contributionValueAtIncrement(increment, totalIncrements) {
+        const incrementFraction = increment/totalIncrements;
+        return initialAmount + (years*incrementFraction)*recurring*frequency;
+    };
+
+    for (let i = 0; i < contributionBarValues.length; i++){
+        const currentValue = contributionValueAtIncrement(i+1, contributionBarValues.length);
+        document.getElementById(contributionBarValues[i]).style.height = heightProportion(currentValue);
+    };
+
+    function interestValueAtIncrement(increment, totalIncrements) {
+        return totalValueAtIncrement(increment, totalIncrements) - contributionValueAtIncrement(increment, totalIncrements);
+    };
+
+    for (let i = 0; i < interestBarValues.length; i++) {
+        const currentValue = interestValueAtIncrement(i+1, interestBarValues.length);
+        document.getElementById(interestBarValues[i]).style.height = heightProportion(currentValue);
+    }
+}*/
